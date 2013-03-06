d1da703f73975451a85573c9a7d9a2ecb8becdf4
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-03-06 00:19:53 -0500

    added notifications

--- lib/guard/pdflatex.rb
+++ lib/guard/pdflatex.rb
@@ -6,19 +6,27 @@ module Guard
   # The PDFLatex Guard automatically compiles your latex files with pdflatex.
   class Pdflatex < Guard
 
+    OPTIONS = {
+      title: 'PDFLatex'
+    }
+
     # Gets called when all files should be regenerated.
     # @raise [:task_has_failed] when stop has failed
-    #def run_all
-    #  run_on_changes(Watcher.match_files(self, Dir.glob('**{,/*/**}/*.tex')))
-    #end
+    def run_all
+      run_on_changes(Watcher.match_files(self, Dir.glob('**{,/*/**}/*.tex')))
+    end
 
     # @param [Array<String>] paths of changed paths and files
     # @raise [:task_has_failed] when stop has failed
     def run_on_changes(paths)
-      ok = paths.map do |path|
-        system *%W[/usr/bin/env pdflatex -halt-on-error -output-directory=#{File.dirname path}], path
-      end.reduce(:&)
-      throw :task_has_failed unless ok
+      failed = false
+      paths.each do |path|
+        unless system *%W[/usr/bin/env pdflatex -halt-on-error -output-directory=#{File.dirname path}], path
+          ::Guard::Notifier.notify "PDFLatex halted on an error in #{path}", OPTIONS.merge(:image => :failed)
+          failed = true
+        end
+      end
+      throw :task_has_failed if failed
     end
 
   end
